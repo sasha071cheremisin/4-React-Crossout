d067a04816dc0faab42c3e4faf8bb6afd5e49506
alex <sasha071cheremisin@mail.ru>
2019-07-09 18:39:56 +0300

    Updates

---
+++ precache-manifest.bc70d1f45aee3a25078bd4f9c8d98e03.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a026ca663fbb2f7154b46369ebff9789",
+    "url": "/4-React-Crossout/index.html"
+  },
+  {
+    "revision": "ec9bc407e3db12676ffa",
+    "url": "/4-React-Crossout/static/js/2.db331e7c.chunk.js"
+  },
+  {
+    "revision": "7501c20a0ec4ef5af5f8",
+    "url": "/4-React-Crossout/static/js/main.a3e6b5e4.chunk.js"
+  },
+  {
+    "revision": "a46df28521627a6c5f5a",
+    "url": "/4-React-Crossout/static/js/runtime~main.3509badc.js"
+  }
+]);
